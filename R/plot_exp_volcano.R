41c3fc9aef43f2137e5c092c23351eee085d1ca1
chloewtz <cwitz@sfr.fr>
2025-04-30 15:37:39 +0200

    volcano

--- R/plot_exp_volcano.R
+++ R/plot_exp_volcano.R
@@ -1,67 +1,93 @@
-library(tidyverse)
-library(ggrepel)
-deseq_volcanoPlot <- function(resLFC,
+#' Volcano Plot for DESeq2 Results
+#'
+#' Creates a volcano plot from DESeq2 differential expression results.
+#'
+#' @param resLFC A data.frame or tibble containing the DESeq2 results with `log2FoldChange`, `padj`, and gene annotations.
+#' @param contrast_name Character. Name of the contrast shown on the x-axis label.
+#' @param lfc_tresh Numeric. Log2 fold-change threshold to define up/down-regulated genes. Default is 2.5.
+#' @param padj_tresh Numeric. Adjusted p-value threshold for significance. Default is 0.05.
+#' @param ngenes_show Integer. Number of top significant genes to label (split equally up/down). Default is 10.
+#' @param gene_col Character. Column name in `resLFC` containing gene names. Default is "external_gene_name".
+#' @param annot_pos Numeric vector of length 2. Position to place DEG annotation text (x, y). Use `Inf` or `-Inf` for corners.
+#'
+#' @return A `ggplot2` object displaying the volcano plot.
+#' @export
+#'
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_manual annotate labs guides theme_minimal
+#' @importFrom ggrepel geom_text_repel
+#' @importFrom dplyr filter slice_min pull case_when mutate
+#' @importFrom rlang .data
+#' @importFrom rlang enquo quo_name
+#'
+plot_exp_volcano <- function(resLFC,
                               contrast_name = "",
                               lfc_tresh = 2.5,
                               padj_tresh = 0.05,
                               ngenes_show = 10,
                               gene_col = "external_gene_name",
                               annot_pos = c(Inf, Inf)) {
-  #-- Get annotations
-  #---- Genes to show
-  genes_up <- resLFC %>%
-    filter(log2FoldChange > lfc_tresh) %>%
-    slice_min(order_by = padj, n = ngenes_show/2) %>%
-    pull({{gene_col}})
-  genes_down <- resLFC %>%
-    filter(log2FoldChange < -lfc_tresh) %>%
-    slice_min(order_by = padj, n = ngenes_show/2) %>%
-    pull({{gene_col}})
+
+  gene_sym <- rlang::ensym(gene_col)
+
+  # Select top upregulated and downregulated genes
+  genes_up <- resLFC |>
+    dplyr::filter(.data$log2FoldChange > lfc_tresh) |>
+    dplyr::slice_min(order_by = .data$padj, n = ngenes_show/2) |>
+    dplyr::pull(!!gene_sym)
+
+  genes_down <- resLFC |>
+    dplyr::filter(.data$log2FoldChange < -lfc_tresh) |>
+    dplyr::slice_min(order_by = .data$padj, n = ngenes_show/2) |>
+    dplyr::pull(!!gene_sym)
+
   genes_label <- c(genes_up, genes_down)
-  #---- x-axis
+
   fc_leg <- paste("FC", contrast_name)
-  #---- Number of DEGs
   ndegs <- sum(resLFC$padj < padj_tresh, na.rm = TRUE)
-  deg_annot <-  paste0("atop(bold('DEGs:'), ", ndegs, "/", nrow(resLFC), ")")
+  deg_annot <- paste0("atop(bold('DEGs:'), ", ndegs, "/", nrow(resLFC), ")")
 
-  #-- Plot data
-  data2plot <- resLFC %>%
-    mutate(lpval = -log10(padj),
-           signif = case_when(
-             log2FoldChange > lfc_tresh & padj < padj_tresh ~ "up",
-             log2FoldChange < -lfc_tresh & padj < padj_tresh ~ "down",
-             padj < 0.05 ~ "signif",
-             TRUE ~ "ns"
-           ),
-           gene_name = pull(resLFC, {{gene_col}}),
-           label = ifelse(gene_name %in% genes_label, gene_name, NA))
+  # Prepare data for plot
+  data2plot <- resLFC |>
+    dplyr::mutate(
+      lpval = -log10(.data$padj),
+      signif = dplyr::case_when(
+        .data$log2FoldChange > lfc_tresh & .data$padj < padj_tresh ~ "up",
+        .data$log2FoldChange < -lfc_tresh & .data$padj < padj_tresh ~ "down",
+        .data$padj < padj_tresh ~ "signif",
+        TRUE ~ "ns"
+      ),
+      gene_name = .data[[gene_col]],
+      label = ifelse(gene_name %in% genes_label, gene_name, NA)
+    )
 
-  #-- Get annotation position params
-  hjust_value <- case_when(
+  # Define annotation positioning
+  hjust_value <- dplyr::case_when(
     annot_pos[1] == Inf ~ 1,
     annot_pos[1] == -Inf ~ -1,
     TRUE ~ 0
   )
-  vjust_value <- case_when(
+
+  vjust_value <- dplyr::case_when(
     annot_pos[2] == Inf ~ 1,
     annot_pos[2] == -Inf ~ -1,
     TRUE ~ 0
   )
 
-  plt <- ggplot(data2plot, aes(log2FoldChange, lpval)) +
-    geom_point(aes(color = signif), alpha = 0.7) +
-    geom_text_repel(aes(label = label), size = 3) +
-    annotate("text", x=annot_pos[1], y=annot_pos[2],
-             vjust=vjust_value, hjust=hjust_value, label=deg_annot,
-             size = 3.5, parse = TRUE) +
-    scale_color_manual(values = c("up" = "#ef8a62",
-                                  "down" = "#67a9cf",
-                                  "signif" = "grey60",
-                                  "ns" = "grey90")) +
-    labs(x = bquote("log"[2]*.(fc_leg)), y = expression(-"log"[10]*"adj. p-value")) +
-    guides(color = "none") +
-    theme_minimal()
+  # Create plot
+  plt <- ggplot2::ggplot(data2plot, ggplot2::aes(.data$log2FoldChange, lpval)) +
+    ggplot2::geom_point(ggplot2::aes(color = signif), alpha = 0.7) +
+    ggrepel::geom_text_repel(ggplot2::aes(label = label), size = 3) +
+    ggplot2::annotate("text", x = annot_pos[1], y = annot_pos[2],
+                      vjust = vjust_value, hjust = hjust_value,
+                      label = deg_annot, parse = TRUE, size = 3.5) +
+    ggplot2::scale_color_manual(values = c("up" = "#ef8a62",
+                                           "down" = "#67a9cf",
+                                           "signif" = "grey60",
+                                           "ns" = "grey90")) +
+    ggplot2::labs(x = bquote("log"[2]*.(fc_leg)), y = expression(-log[10]*" adj. p-value")) +
+    ggplot2::guides(color = "none") +
+    ggplot2::theme_minimal()
 
+  # Return the plot object
   return(plt)
-
 }
