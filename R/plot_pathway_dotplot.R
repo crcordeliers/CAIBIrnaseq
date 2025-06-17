4985402c877dfc01b945545f02c5e9a9df99ee70
chloewtz <cwitz@sfr.fr>
2025-06-17 15:38:11 +0200

    Update plot_pathway_dotplot.R

--- R/plot_pathway_dotplot.R
+++ R/plot_pathway_dotplot.R
@@ -24,15 +24,22 @@
 #'
 #' @export
 #'
-plot_pathway_dotplot <- function(exp_data, score_name = "resultsORA", top_n = 10, maxPval = 0.05) {
+plot_pathway_dotplot <- function(exp_data, score_name = "resultsORA", top_n = 10, maxPval = 0.05, usePval = TRUE) {
   results <- S4Vectors::metadata(exp_data)[[score_name]]
 
   if ("PValue" %in% colnames(results)) {
     # ORA format
+    if (usePval) {
+      results <- results |>
+        dplyr::arrange(PAdj) |>
+        dplyr::filter(PAdj < maxPval) |>
+        dplyr::slice_head(n = top_n)
+    } else {
+      results <- results |>
+        dplyr::arrange(desc(PAdj)) |>
+        dplyr::slice_head(n = top_n)
+    }
     results <- results |>
-      dplyr::arrange(PAdj) |>
-      dplyr::filter(PAdj < maxPval) |>
-      dplyr::slice_head(n = top_n) |>
       dplyr::mutate(
         GeneRatioNum = as.numeric(sub("/.*", "", GeneRatio)) / as.numeric(sub(".*/", "", GeneRatio)),
         Pathway = factor(Pathway, levels = rev(Pathway)),
@@ -41,10 +48,17 @@ plot_pathway_dotplot <- function(exp_data, score_name = "resultsORA", top_n = 10
       )
   } else if ("pval" %in% colnames(results)) {
     # FGSEA format
+    if (usePval) {
+      results <- results |>
+        dplyr::arrange(padj) |>
+        dplyr::filter(padj < maxPval) |>
+        dplyr::slice_head(n = top_n)
+    } else {
+      results <- results |>
+        dplyr::arrange(desc(padj)) |>
+        dplyr::slice_head(n = top_n)
+    }
     results <- results |>
-      dplyr::arrange(padj) |>
-      dplyr::filter(padj < maxPval) |>
-      dplyr::slice_head(n = top_n) |>
       dplyr::mutate(
         GeneRatioNum = size / max(size),
         Pathway = factor(pathway, levels = rev(pathway)),
@@ -55,26 +69,32 @@ plot_pathway_dotplot <- function(exp_data, score_name = "resultsORA", top_n = 10
     stop("Unsupported results format: expected columns `PAdj` or `padj`.")
   }
 
-  ggplot2::ggplot(results, ggplot2::aes(x = logpadj, y = Pathway, size = Size, color = logpadj)) +
+  if (usePval) {
+    p <- ggplot2::ggplot(results, ggplot2::aes(x = logpadj, y = Pathway, size = Size, color = logpadj)) +
+      ggplot2::scale_x_continuous(labels = function(x) 10^-x, breaks = scales::pretty_breaks(n = 5)) +
+      ggplot2::scale_color_gradient(
+        low = "#df6664", high = "#387eb9",
+        labels = function(x) 10^-x,
+        breaks = scales::pretty_breaks(n = 6),
+        limits = c(-log10(maxPval), max(results$logpadj))
+      ) +
+      ggplot2::labs(x = "Adjusted p-value", color = "Adjusted p-value")
+  } else {
+    p <- ggplot2::ggplot(results, ggplot2::aes(x = Size, y = Pathway, size = Size)) +
+      ggplot2::geom_point(color = "#387eb9") +
+      ggplot2::labs(x = "Gene Ratio", color = NULL)
+  }
+
+  p +
     ggplot2::geom_point() +
-    ggplot2::scale_x_continuous(labels = function(x) 10^-x, breaks = scales::pretty_breaks(n = 5)) +
-    ggplot2::scale_color_gradient(
-      low = "#df6664", high = "#387eb9",
-      labels = function(x) 10^-x,
-      breaks = scales::pretty_breaks(n = 6),
-      limits = c(-log10(maxPval), max(results$logpadj))
-    ) +
     ggplot2::scale_size_continuous(range = c(2, 10)) +
     ggplot2::scale_y_discrete(labels = function(x) stringr::str_wrap(stringr::str_replace_all(x, "_", " "), width = 30)) +
     ggplot2::ggtitle("Pathway Analysis") +
-    ggplot2::labs(
-      y = "Pathway", x = "Adjusted p-value",
-      color = "Adjusted p-value", size = "Gene Ratio"
-    ) +
+    ggplot2::labs(y = "Pathway", size = "Gene Ratio") +
     ggplot2::theme(
       plot.title = ggplot2::element_text(size = 12, face = "bold"),
       axis.text.y = ggplot2::element_text(size = 10),
-      axis.text.x = ggplot2::element_text(size = 10, angle = 45, hjust = 1),
+      axis.text.x = ggplot2::element_text(size = 10, angle = ifelse(usePval, 45, 0), hjust = 1),
       axis.title.x = ggplot2::element_text(size = 12),
       axis.title.y = ggplot2::element_text(size = 12),
       panel.background = ggplot2::element_blank(),
